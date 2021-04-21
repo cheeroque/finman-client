0b5bc50afb1cd5f5863560c73d7af5ec54a37007
Yury Feshchenko <yu.feshenko@ekf.su>
2021-04-21 17:52:20 +0300

    Added server sorting & pagination

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <b-container tag="main">
-    <b-table :fields="fields" :items="records"></b-table>
+    <b-table :fields="fields" :items="records.data" @sort-changed="onTableSort"></b-table>
   </b-container>
 </template>
 
@@ -23,8 +23,22 @@ export default {
       ]
     }
   },
+  watchQuery: true,
   async fetch() {
-    this.records = await this.$http.$get(`${process.env.API_URL}/records`)
+    this.records = await this.$http.$get(`${process.env.API_URL}/records?${this.query}`)
+  },
+  computed: {
+    query() {
+      return Object.keys(this.$route.query)
+        .map((key) => `${key}=${this.$route.query[key]}`)
+        .join('&')
+    }
+  },
+  methods: {
+    onTableSort(event) {
+      this.$router.push({ query: { orderBy: event.sortBy, order: event.sortDesc ? 'DESC' : 'ASC' } })
+      this.$fetch()
+    }
   }
 }
 </script>
