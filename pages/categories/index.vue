db1ddf185f47bd2a024b2f574be5884e167d11c2
Yury Feshchenko <yu.feshenko@ekf.su>
2021-04-21 15:29:21 +0300

    Added base records & categories pages

---
+++ pages/categories/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-container tag="main">
+    <b-table :fields="fields" :items="categories"></b-table>
+  </b-container>
+</template>
+
+<script>
+import { BTable } from 'bootstrap-vue'
+
+export default {
+  components: {
+    BTable
+  },
+  data() {
+    return {
+      categories: [],
+      fields: [
+        { key: 'id', label: 'ID', sortable: true },
+        { key: 'name', label: 'Имя', sortable: true },
+        { key: 'slug', label: 'URL', sortable: true },
+        { key: 'is_income', label: 'Доход', sortable: true },
+        { key: 'edit', label: 'Редактировать' }
+      ]
+    }
+  },
+  async fetch() {
+    this.categories = await this.$http.$get(`${process.env.API_URL}/categories`)
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,30 @@
 <template>
-  <main></main>
+  <b-container tag="main">
+    <b-table :fields="fields" :items="records"></b-table>
+  </b-container>
 </template>
+
+<script>
+import { BTable } from 'bootstrap-vue'
+
+export default {
+  components: {
+    BTable
+  },
+  data() {
+    return {
+      records: [],
+      fields: [
+        { key: 'created_at', label: 'Дата', sortable: true },
+        { key: 'sum', label: 'Сумма', sortable: true },
+        { key: 'category_id', label: 'Категория', sortable: true },
+        { key: 'note', label: 'Комментарий' },
+        { key: 'edit', label: 'Редактировать' }
+      ]
+    }
+  },
+  async fetch() {
+    this.records = await this.$http.$get(`${process.env.API_URL}/records`)
+  }
+}
+</script>
