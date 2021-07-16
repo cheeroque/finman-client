40954945dbe4e0f861d9133aef3e86b8e3d3e169
Yury Feshchenko <yu.feshenko@ekf.su>
2021-07-16 16:32:04 +0300

    Added pagination-nav component

---
+++ components/global/PaginationNav.vue
@@ -0,0 +1,40 @@
+<template>
+  <b-pagination-nav
+    :align="align"
+    :link-gen="linkGen"
+    :number-of-pages="numberOfPages"
+    :size="size"
+    first-number
+    last-number
+    use-router
+  />
+</template>
+
+<script>
+import { BPaginationNav } from 'bootstrap-vue'
+
+export default {
+  components: {
+    BPaginationNav
+  },
+  props: {
+    align: {
+      type: String,
+      default: null
+    },
+    numberOfPages: {
+      type: [Number, String],
+      default: 1
+    },
+    size: {
+      type: String,
+      default: null
+    }
+  },
+  methods: {
+    linkGen(pageNumber) {
+      return pageNumber === 1 ? '?' : { query: { ...this.$route.query, page: pageNumber } }
+    }
+  }
+}
+</script>
