d6990857e87bd224f6b5e0e92d0ee659a3329abd
Yury Feschenko <ronerycamel@gmail.com>
2021-09-09 20:09:25 +0300

    Added login page styles

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,3 +1,15 @@
 <template>
-  <Nuxt />
+  <div class="app-wrapper">
+    <Nuxt />
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.app-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem;
+  min-height: 100vh;
+}
+</style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,15 +1,18 @@
 <template>
-  <b-container>
-    <form @submit.prevent="onSubmit">
-      <div>
-        <input v-model="form.name" type="text" required />
+  <form class="form-login" @submit.prevent="onSubmit">
+    <h3 class="text-center mb-24">Авторизация</h3>
+    <f-form-group label="Логин">
+      <f-input v-model="form.name" type="text" required />
+    </f-form-group>
+    <f-form-group label="Пароль" class="mb-32">
+      <f-input v-model="form.password" type="password" required />
+    </f-form-group>
+    <div class="row">
+      <div class="col-8 col-start-3">
+        <button class="btn btn-block btn-lg btn-success" type="submit">Войти</button>
       </div>
-      <div>
-        <input v-model="form.password" type="password" required />
-      </div>
-      <button type="submit">Войти</button>
-    </form>
-  </b-container>
+    </div>
+  </form>
 </template>
 
 <script>
@@ -40,3 +43,10 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.form-login {
+  flex: 1 1 auto;
+  max-width: 320px;
+}
+</style>
