8416104c495573328e5eb7fdde669f34220328a2
Yury Feschenko <ronerycamel@gmail.com>
2022-03-06 23:29:00 +0300

    Added form group & form input

---
+++ components/form/Group.vue
@@ -0,0 +1,33 @@
+<template>
+  <div ref="group" class="form-group" role="group">
+    <label v-if="label" :for="controlId" class="form-group-label">
+      <span>{{ label }}</span>
+    </label>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: null,
+    },
+  },
+  computed: {
+    controlId() {
+      return `form-control-${this._uid}`
+    },
+  },
+  mounted() {
+    this.setControlId()
+  },
+  methods: {
+    setControlId() {
+      const control = this.$refs.group.querySelector('.form-control')
+      if (control) control.setAttribute('id', this.controlId)
+    },
+  },
+}
+</script>
---
+++ components/form/Input.vue
@@ -0,0 +1,37 @@
+<template>
+  <input
+    v-model="localValue"
+    :placeholder="placeholder"
+    :type="type"
+    class="form-control"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    placeholder: {
+      type: String,
+      default: null,
+    },
+    type: {
+      type: String,
+      default: 'text',
+    },
+    value: {
+      type: [Number, String],
+      default: null,
+    },
+  },
+  computed: {
+    localValue: {
+      get() {
+        return this.value
+      },
+      set(value) {
+        this.$emit('input', value)
+      },
+    },
+  },
+}
+</script>
