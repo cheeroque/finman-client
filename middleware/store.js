4a71b0c52492cbabcba99f7a8c167d04eec70639
Yury Feschenko <ronerycamel@gmail.com>
2022-03-27 13:56:58 +0300

    Fixed logging in with wrong token

--- middleware/store.js
+++ middleware/store.js
@@ -1,10 +1,19 @@
 export default async function ({ $auth, store }) {
   /* Get global data */
   if ($auth.$state.loggedIn) {
-    await store.dispatch('fetchTotal')
-    await store.dispatch('fetchLatestShapshot')
-    await store.dispatch('fetchCategories')
-    await store.dispatch('fetchCurrentMonthRecords')
+    try {
+      await store.dispatch('fetchTotal')
+      await store.dispatch('fetchLatestShapshot')
+      await store.dispatch('fetchCategories')
+      await store.dispatch('fetchCurrentMonthRecords')
+    } catch (error) {
+      if (error?.response?.status === 401) {
+        await $auth.logout()
+        $auth.setUser(false)
+      } else {
+        throw error
+      }
+    }
   }
 
   /* Close navigation drawer */
