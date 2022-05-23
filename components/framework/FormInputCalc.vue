aa652c6414736f695b49829ef4dac6b2d234d637
Yury Feshchenko <yu.feshenko@ekf.su>
2022-05-23 14:40:27 +0300

    Added inputmode & pattern to FormInputCalc

--- components/framework/FormInputCalc.vue
+++ components/framework/FormInputCalc.vue
@@ -6,6 +6,8 @@
       'form-control-valid': state === true,
       'form-control-invalid': state === false,
     }"
+    inputmode="numeric"
+    pattern="[\d+-]+"
     placeholder="0"
     type="text"
     class="form-control"
